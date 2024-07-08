35a488b21f8c5a987bf3a8041e71970b7076daa0
Lobzter <olowo15tomiloba@gmail.com>
2024-07-08 15:01:00 +0100

    First commit

---
+++ src/test/java/iolobzter/web/app/courseProject/CourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package iolobzter.web.app.courseProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
