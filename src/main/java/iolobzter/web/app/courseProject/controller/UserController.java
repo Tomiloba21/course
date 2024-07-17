5b3f962a2c95d546205154e4dc0c61a27cbb9a54
Lobzter <olowo15tomiloba@gmail.com>
2024-07-17 03:28:29 +0100

    stil on security part1

--- src/main/java/iolobzter/web/app/courseProject/controller/UserController.java
+++ src/main/java/iolobzter/web/app/courseProject/controller/UserController.java
@@ -6,20 +6,24 @@ import iolobzter.web.app.courseProject.entity.User;
 import iolobzter.web.app.courseProject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping
 public class UserController {
 
     private final UserService userService;
 
+    @PostMapping("/auth")
     public  ResponseEntity<String> authenticate(@RequestBody  UserAuth authDetails){
         return ResponseEntity.ok(userService.authenticate(authDetails));
     }
-
+    @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody UserReg regDetails){
         return ResponseEntity.ok(userService.register(regDetails));
     }
--- src/main/java/iolobzter/web/app/courseProject/service/UserService.java
+++ src/main/java/iolobzter/web/app/courseProject/service/UserService.java
@@ -8,9 +8,13 @@ import iolobzter.web.app.courseProject.entity.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +28,13 @@ public class UserService {
     private  final UserDetailsService userDetailsService;
     private final AuthenticationManager authenticationManager;
     public String authenticate(UserAuth authDetails) {
-            User user = userDetailsService.loadUserByUsername(authDetails.getEmail())
+            User user = (User) userDetailsService.loadUserByUsername(authDetails.getEmail());
+            if (user == null ){
+                throw new UsernameNotFoundException("User With the provided Username is not found");
+            }
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authDetails.getEmail(), authDetails.getPassword()));
+            return  jwtService.generateToken(Map.of("sub", authDetails.getEmail()));
     }
 
     public User register(UserReg regDetails) {
